828da09f5414883f59d0419423f423c39fdadad0
thobekabovana <166359793+thobekabovana@users.noreply.github.com>
2024-07-09 11:14:14 +0200

    first commit
    flexbox

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import "./style.css"
+
+function App() {
+  return (
+    <>
+    <div className="flex-box-container">
+      <div className="flex-item box1" ></div>
+      <div className="flex-item box2" ></div>
+      <div className="flex-item box3" ></div>
+      <div className="flex-item box4" ></div>
+      <div className="flex-item box5" ></div>
+      <div className="flex-item box6" ></div>
+      <div className="flex-item box7" ></div>
+      <div className="flex-item box8" ></div>
+      <div className="flex-item box9" ></div>
+      <div className="flex-item box10" ></div>
+    </div>
+    </>
+  )
+}
+export default App
